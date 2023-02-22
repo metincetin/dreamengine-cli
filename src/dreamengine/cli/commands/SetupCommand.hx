e316570060715106721e5b098dde549d02dfdc19
Metin Çetin <metinc309@gmail.com>
2023-02-22 21:36:57 +0300

    Added setup command

--- src/dreamengine/cli/commands/SetupCommand.hx
+++ src/dreamengine/cli/commands/SetupCommand.hx
@@ -14,10 +14,18 @@ class SetupCommand extends comma.Command {
 	}
 
 	override function onExecuted(app:CliApp, values:Array<String>, options:ParsedOptions) {
-       app.println("Setting up dreamengine in path"); 
-
-
-	   var path = "/usr/local/bin/";
-	   File.saveContent(Path.join([path, "dreamengine"]), "#!/bin/sh\nhaxelib run dreamengine-cli \"$@\"");
-    }
+		switch(Sys.systemName()){
+			case "Linux":
+				var pr = new sys.io.Process(Path.join([Sys.getCwd(), 'scripts/setup.sh']));
+				var code = pr.exitCode();
+				if (code != 0){
+					app.println('Error: ${code} - ${pr.stdout.readAll().toString()}');
+				}else{
+					app.println("Set up dreamengine in path");
+				}
+				pr.close();
+			case "Mac":
+			case "Windows":
+		}
+	}
 }
