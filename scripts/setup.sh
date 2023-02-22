4e2b0f833a1ed8f925579e87900824a403933f01
Metin Çetin <metinc309@gmail.com>
2023-02-22 22:25:10 +0300

    Removed setup.sh

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo rm /usr/local/bin/dreamengine
-
-cm='printf "#!/bin/sh\nhaxelib run dreamengine-cli \"\$@\"" >> /usr/local/bin/dreamengine'
-
-sudo sh -c "$cm"
-
-sudo chmod +x /usr/local/bin/dreamengine
